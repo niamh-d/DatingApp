546f38114959d6ec8a2b911c455fbb8cf743931d
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-12-02 22:24:12 +0200

    register dto and unique email pre-registration check

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,23 +1,31 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 public class AccountController(AppDbContext context) : BaseApiController
 {
     [HttpPost("register")]
-    public async Task<ActionResult<AppUser>> Register(string displayName, string email, string password)
+    public async Task<ActionResult<AppUser>> Register(RegisterDto dto)
     {
+
+        if (await EmailExists(dto.Email))
+        {
+            return BadRequest("Email already exists");
+        }
+
         using var hmac = new HMACSHA512();
 
         var user = new AppUser
         {
-            DisplayName = displayName,
-            Email = email,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            DisplayName = dto.DisplayName,
+            Email = dto.Email,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(dto.Password)),
             PaswordSalt = hmac.Key
         };
 
@@ -25,5 +33,10 @@ public class AccountController(AppDbContext context) : BaseApiController
         await context.SaveChangesAsync();
 
         return user;
+    }
+
+    private async Task<bool> EmailExists(string email)
+    {
+        return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
     }
 }
