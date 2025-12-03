78480abfd279091d7f61078e87c1f18d5a0feb0d
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-12-03 18:53:08 +0200

    login logout logic

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../../core/services/account-service';
 
 @Component({
   selector: 'app-nav',
@@ -8,9 +9,24 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './nav.css',
 })
 export class Nav {
+  private accountService = inject(AccountService);
   protected creds: any = {};
+  protected loggedIn = signal(false);
 
   login() {
-    console.log(this.creds);
+    this.accountService.login(this.creds).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.loggedIn.set(true);
+        this.creds = {};
+      },
+      error: (err) => {
+        alert(err.message);
+      },
+    });
+  }
+
+  logout() {
+    this.loggedIn.set(false);
   }
 }
