8605c211c7fd44e768b03a0c9ed634a698a6194c
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-12-04 12:37:14 +0200

    programatic routing

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,13 +10,14 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './nav.css',
 })
 export class Nav {
+  private router = inject(Router);
   protected accountService = inject(AccountService);
   protected creds: any = {};
 
   login() {
     this.accountService.login(this.creds).subscribe({
       next: (res) => {
-        console.log(res);
+        this.router.navigateByUrl('/members');
         this.creds = {};
       },
       error: (err) => {
@@ -27,5 +28,6 @@ export class Nav {
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
