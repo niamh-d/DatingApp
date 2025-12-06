c62183ab66c29cdd22d42595f0e4bbc872732688
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-12-06 13:50:45 +0200

    server error page

--- client/src/shared/errors/server-error/server-error.ts
+++ client/src/shared/errors/server-error/server-error.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiError } from '../../../types/error';
 
 @Component({
   selector: 'app-server-error',
   imports: [],
   templateUrl: './server-error.html',
-  styleUrl: './server-error.css'
+  styleUrl: './server-error.css',
 })
 export class ServerError {
+  protected error: ApiError;
+  private router = inject(Router);
+  protected showDetails = false;
 
+  constructor() {
+    const navigation = this.router.currentNavigation();
+    this.error = navigation?.extras?.state?.['error'];
+  }
+
+  detailsToogle() {
+    this.showDetails = !this.showDetails;
+  }
 }
