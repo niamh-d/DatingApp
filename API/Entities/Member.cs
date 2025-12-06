12d7a2ba0fe6e5fc764b9da7cb73c7ab0cfddabe
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-12-06 17:03:35 +0200

    remove some navigation props in entities fromm json response

--- API/Entities/Member.cs
+++ API/Entities/Member.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace API.Entities
 {
@@ -14,7 +15,9 @@ namespace API.Entities
         public string? Description { get; set; }
         public required string City { get; set; }
         public required string Country { get; set; }
+        [JsonIgnore]
         public List<Photo> Photos { get; set; } = [];
+        [JsonIgnore]
         [ForeignKey(nameof(Id))]
         public AppUser User { get; set; } = null!;
     }
--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace API.Entities
 {
     public class Photo
@@ -5,6 +7,7 @@ namespace API.Entities
         public int Id { get; set; }
         public required string Url { get; set; }
         public string? PublicId { get; set; }
+        [JsonIgnore]
         public Member Member { get; set; } = null!;
         public string MemberId { get; set; } = null!;
     }
