e5445ee39c4ae5d744fb8bb332560a5d6b5e5f33
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-12-06 14:27:04 +0200

    member and photo entities

---
+++ API/Entities/Photo.cs
@@ -0,0 +1,10 @@
+namespace API.Entities
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public required string Url { get; set; }
+        public string? PublicId { get; set; }
+        public Member Member { get; set; } = null!;
+    }
+}
