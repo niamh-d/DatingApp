ac98f2b68ab55d676f156ee22682577b16d4b7c0
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-12-07 15:55:07 +0200

    can update user in api

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,3 +1,5 @@
+using System.Security.Claims;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -31,6 +33,29 @@ namespace API.Controllers
         public async Task<ActionResult<IReadOnlyList<Photo>>> GetPhotosForMember(string id)
         {
             return Ok(await memberRepository.GetPhotosForMemberAsync(id));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateMember(MemberUpdateDto dto)
+        {
+            var memberId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (memberId == null) return BadRequest("Oops – no id found in the token");
+
+            var member = await memberRepository.GetMemberByIdAsync(memberId);
+
+            if (member == null) return BadRequest("Member not found");
+
+            member.DisplayName = dto.DisplayName ?? member.DisplayName;
+            member.Description = dto.Description ?? member.Description;
+            member.City = dto.City ?? member.City;
+            member.Country = dto.Country ?? member.Country;
+
+            memberRepository.Update(member);
+
+            if (await memberRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update member");
         }
     }
 }
