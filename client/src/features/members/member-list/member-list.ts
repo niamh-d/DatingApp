32d9f1329b0f008949726a3bffd5db6e5e559470
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-12-10 21:16:02 +0200

    can filter by age and gender in client from filter modal

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -45,7 +45,8 @@ export class MemberList implements OnInit {
   }
 
   onFilterChange(data: MemberParams) {
-    console.log('Modal data', data);
+    this.memberParams = data;
+    this.loadMembers();
   }
 
   resetFilters() {
