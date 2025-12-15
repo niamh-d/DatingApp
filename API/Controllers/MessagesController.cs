8665f240c7aec73311385e074d216f2942088875
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-12-15 21:03:50 +0200

    can create message in backend

---
+++ API/Controllers/MessagesController.cs
@@ -0,0 +1,33 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class MessagesController(IMessageRepository messageRepository, IMemberRepository memberRepository) : BaseApiController
+    {
+        [HttpPost]
+        public async Task<ActionResult<MessageDto>> CreateMessage([FromBody] CreateMessageDto createMessageDto)
+        {
+            var sender = await memberRepository.GetMemberByIdAsync(User.GetMemberId());
+            var recipient = await memberRepository.GetMemberByIdAsync(createMessageDto.RecipientId);
+
+            if (sender == null || recipient == null || sender.Id == createMessageDto.RecipientId) return BadRequest("Cannot send message");
+
+            var message = new Message
+            {
+                SenderId = sender.Id,
+                RecipientId = recipient.Id,
+                Content = createMessageDto.Content
+            };
+
+            messageRepository.AddMessage(message);
+
+            if (await messageRepository.SaveAllAsync()) return message.ToDto();
+
+            return BadRequest("Failed to send message");
+        }
+    }
+}
---
+++ API/Interfaces/IMessageRepository.cs
@@ -0,0 +1,21 @@
+using API.DTOs;
+using API.Entities;
+using API.Helpers;
+
+namespace API.Interfaces
+{
+    public interface IMessageRepository
+    {
+        void AddMessage(Message message);
+
+        void DeleteMessage(Message message);
+
+        Task<Message?> GetMessage(string id);
+
+        Task<PaginatedResult<MessageDto>> GetMessagesForMember();
+
+        Task<IReadOnlyList<MessageDto>> GetMessageThread(string curremtMemberId, string recipientId);
+
+        Task<bool> SaveAllAsync();
+    }
+}
