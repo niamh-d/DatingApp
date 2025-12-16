7c1e53d4f6ffe7d6e81df93526c3daaf9ae8f55a
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-12-16 12:55:19 +0200

    rendering message content in messages component

--- client/src/features/messages/messages.ts
+++ client/src/features/messages/messages.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MessageService } from '../../core/services/message-service';
+import { PaginatedResult } from '../../types/pagination';
+import { Message } from '../../types/message';
 
 @Component({
   selector: 'app-messages',
   imports: [],
   templateUrl: './messages.html',
-  styleUrl: './messages.css'
+  styleUrl: './messages.css',
 })
-export class Messages {
+export class Messages implements OnInit {
+  private messageService = inject(MessageService);
+  protected container = 'inbox';
+  protected pageNumber = 1;
+  protected pageSize = 10;
+  protected paginatedMessages = signal<PaginatedResult<Message> | null>(null);
 
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messageService.getMessages(this.container, this.pageNumber, this.pageSize).subscribe({
+      next: (res) => this.paginatedMessages.set(res),
+    });
+  }
 }
