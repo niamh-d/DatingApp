f094c8a11a72b7602b1b0780f4907d4a4a5bda08
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-12-16 14:33:20 +0200

    rendering table of messages for inbox and outbox

--- client/src/features/messages/messages.ts
+++ client/src/features/messages/messages.ts
@@ -2,19 +2,28 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { MessageService } from '../../core/services/message-service';
 import { PaginatedResult } from '../../types/pagination';
 import { Message } from '../../types/message';
+import { Paginator } from '../../shared/paginator/paginator';
+import { RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-messages',
-  imports: [],
+  imports: [Paginator, RouterLink, DatePipe],
   templateUrl: './messages.html',
   styleUrl: './messages.css',
 })
 export class Messages implements OnInit {
   private messageService = inject(MessageService);
   protected container = 'inbox';
+  protected fetchedContainer = 'inbox';
   protected pageNumber = 1;
   protected pageSize = 10;
   protected paginatedMessages = signal<PaginatedResult<Message> | null>(null);
+
+  tabs = [
+    { label: 'Inbox', value: 'inbox' },
+    { label: 'Outbox', value: 'outbox' },
+  ];
 
   ngOnInit(): void {
     this.loadMessages();
@@ -22,7 +31,26 @@ export class Messages implements OnInit {
 
   loadMessages() {
     this.messageService.getMessages(this.container, this.pageNumber, this.pageSize).subscribe({
-      next: (res) => this.paginatedMessages.set(res),
+      next: (res) => {
+        this.paginatedMessages.set(res);
+        this.fetchedContainer = this.container;
+      },
     });
+  }
+
+  get isInbox() {
+    return this.fetchedContainer === 'inbox';
+  }
+
+  setContainer(container: string) {
+    this.container = container;
+    this.pageNumber = 1;
+    this.loadMessages();
+  }
+
+  onPageChange(event: { pageNumber: number; pageSize: number }) {
+    this.pageNumber = event.pageNumber;
+    this.pageSize = event.pageSize;
+    this.loadMessages();
   }
 }
