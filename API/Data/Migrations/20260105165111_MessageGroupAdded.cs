a87eb944b90dd33b046aa5c9a8af513bf2f7c659
Niamh <85398097+niamh-d@users.noreply.github.com>
2026-01-05 18:53:21 +0200

    entities and methods related to groups and connections in message repository

---
+++ API/Data/Migrations/20260105165111_MessageGroupAdded.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MessageGroupAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Connections",
+                columns: table => new
+                {
+                    ConnectionId = table.Column<string>(type: "TEXT", nullable: false),
+                    UserId = table.Column<string>(type: "TEXT", nullable: false),
+                    GroupName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Connections", x => x.ConnectionId);
+                    table.ForeignKey(
+                        name: "FK_Connections_Groups_GroupName",
+                        column: x => x.GroupName,
+                        principalTable: "Groups",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "admin-id",
+                column: "ConcurrencyStamp",
+                value: "c092b84e-3ca6-4917-a01a-9dd5dc6e56f7");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "member-id",
+                column: "ConcurrencyStamp",
+                value: "2c33fd56-3870-4a0f-8290-12277b60efd2");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "moderator-id",
+                column: "ConcurrencyStamp",
+                value: "b227671c-96b6-4b51-b7d8-132b9664096a");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connections_GroupName",
+                table: "Connections",
+                column: "GroupName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Connections");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "admin-id",
+                column: "ConcurrencyStamp",
+                value: "d584cfc7-9481-4da5-945c-bf6d291ca443");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "member-id",
+                column: "ConcurrencyStamp",
+                value: "9a40d3c0-0a06-49f0-bd72-88942e08795c");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "moderator-id",
+                column: "ConcurrencyStamp",
+                value: "db6bcb8a-aaf2-4a47-954b-41152e41e55c");
+        }
+    }
+}
