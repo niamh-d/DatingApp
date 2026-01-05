dfc6e89b221a316c0958e7c6d7bbc566fe95d2de
Niamh <85398097+niamh-d@users.noreply.github.com>
2026-01-05 19:21:08 +0200

    messages in thread marked as read when recipient has connection in group

--- client/src/features/members/member-messages/member-messages.ts
+++ client/src/features/members/member-messages/member-messages.ts
@@ -1,4 +1,13 @@
-import { Component, effect, ElementRef, inject, OnInit, signal, ViewChild } from '@angular/core';
+import {
+  Component,
+  effect,
+  ElementRef,
+  inject,
+  OnDestroy,
+  OnInit,
+  signal,
+  ViewChild,
+} from '@angular/core';
 import { MessageService } from '../../../core/services/message-service';
 import { MemberService } from '../../../core/services/member-service';
 import { Message } from '../../../types/message';
@@ -14,7 +23,7 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './member-messages.html',
   styleUrl: './member-messages.css',
 })
-export class MemberMessages implements OnInit {
+export class MemberMessages implements OnInit, OnDestroy {
   @ViewChild('messageEndRef') messageEndRef!: ElementRef;
   protected messageService = inject(MessageService);
   private memberService = inject(MemberService);
@@ -29,6 +38,9 @@ export class MemberMessages implements OnInit {
         this.scrollToBottom();
       }
     });
+  }
+  ngOnDestroy(): void {
+    this.messageService.stopHubConnection();
   }
 
   ngOnInit(): void {
