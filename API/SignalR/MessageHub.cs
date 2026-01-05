4e26a6b33b4af7f0bf3254478f8ead3f86c70dcc
Niamh <85398097+niamh-d@users.noreply.github.com>
2026-01-05 17:59:01 +0200

    message hub

---
+++ API/SignalR/MessageHub.cs
@@ -0,0 +1,66 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace API.SignalR
+{
+    [Authorize]
+    public class MessageHub(IMessageRepository messageRepository, IMemberRepository memberRepository) : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            var httpContext = Context.GetHttpContext();
+            var otherUser = httpContext?.Request?.Query["userId"].ToString()
+                ?? throw new HubException("Other user not found");
+            var groupName = GetGroupName(GetUserId(), otherUser);
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            var messages = await messageRepository.GetMessageThread(GetUserId(), otherUser);
+
+            await Clients.Group(groupName).SendAsync("ReceiveMessageThread", messages);
+        }
+
+        public async Task SendMessage(CreateMessageDto createMessageDto)
+        {
+            var sender = await memberRepository.GetMemberByIdAsync(GetUserId());
+            var recipient = await memberRepository.GetMemberByIdAsync(createMessageDto.RecipientId);
+
+            if (sender == null || recipient == null || sender.Id == createMessageDto.RecipientId)
+                throw new HubException("Cannot send message");
+
+            var message = new Message
+            {
+                SenderId = sender.Id,
+                RecipientId = recipient.Id,
+                Content = createMessageDto.Content
+            };
+
+            messageRepository.AddMessage(message);
+
+            if (await messageRepository.SaveAllAsync())
+            {
+                var group = GetGroupName(sender.Id, recipient.Id);
+                await Clients.Group(group).SendAsync("NewMessage", message.ToDto());
+            }
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        private static string GetGroupName(string? caller, string? other)
+        {
+            var stringCompare = string.CompareOrdinal(caller, other) < 0;
+            return stringCompare ? $"{caller}-{other}" : $"{other}-{caller}";
+        }
+
+        private string GetUserId()
+        {
+            return Context.User?.GetMemberId() ?? throw new HubException("Cannot get member id");
+        }
+    }
+}
