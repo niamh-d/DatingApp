494dc54d52c578fbb08f6989cdb58d6001fba10e
Niamh <85398097+niamh-d@users.noreply.github.com>
2026-01-06 19:32:24 +0200

    confirm dialog

---
+++ client/src/core/services/confirm-dialog-service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ConfirmDialog } from '../../shared/confirm-dialog/confirm-dialog';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConfirmDialogService {
+  private dialogComponent?: ConfirmDialog;
+
+  register(component: ConfirmDialog) {
+    this.dialogComponent = component;
+  }
+
+  confirm(message: string): Promise<boolean> {
+    if (!this.dialogComponent) throw new Error('Confirm dialog component not registered');
+    return this.dialogComponent.open(message);
+  }
+}
---
+++ client/src/shared/confirm-dialog/confirm-dialog.ts
@@ -0,0 +1,43 @@
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
+import { ConfirmDialogService } from '../../core/services/confirm-dialog-service';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  imports: [],
+  templateUrl: './confirm-dialog.html',
+  styleUrl: './confirm-dialog.css',
+})
+export class ConfirmDialog {
+  @ViewChild('dialogRef') dialogRef!: ElementRef<HTMLDialogElement>;
+  message = 'Are you sure?';
+  private resolver: ((value: boolean) => void) | null = null;
+
+  constructor() {
+    inject(ConfirmDialogService).register(this);
+  }
+
+  open(message: string): Promise<boolean> {
+    this.message = message;
+    this.dialogRef.nativeElement.showModal();
+
+    return new Promise((resolve) => {
+      this.resolver = resolve;
+    });
+  }
+
+  confirm() {
+    this.closeDialog();
+    this.resolver?.(true);
+    this.resolver = null;
+  }
+
+  cancel() {
+    this.closeDialog();
+    this.resolver?.(false);
+    this.resolver = null;
+  }
+
+  private closeDialog() {
+    this.dialogRef.nativeElement.close();
+  }
+}
